3f77fa80b91b2ba5ff8945f9f94d8e7da37a3250
TAVU - Robert <robertperic@outlook.com>
2021-07-28 13:34:43 +0200

    Initial commit

---
+++ examples/Receiver/Receiver.ino
@@ -0,0 +1,40 @@
+/**
+ **************************************************
+ *
+ * @file        Receiver.ino
+ * @brief       Receiver Example for RF433 antena
+ *
+ *
+ *
+ * @authors     @ soldered.com, Robert Peric
+ * @link        www.solde.red/333067
+ ***************************************************/
+
+#include "RF433-SOLDERED.h"
+#include "SPI.h"
+// pin 11 for data
+ 
+RH_ASK driver;  
+uint8_t buff[2];  
+uint8_t bufflen = sizeof(buff);
+uint16_t value; // Variable that will hold received value
+ 
+void setup() {
+  Serial.begin(9600);
+  if (!driver.init()) { // We check to see if antena is initialised
+    Serial.println("init failed");
+  }
+  pinMode(13, OUTPUT);  // LED as output
+}
+ 
+void loop() {
+  if (driver.recv(buff, &bufflen) ) { /
+    value = buff[0];  
+    value |= (buff[1] < 512); 
+    digitalWrite(13, HIGH);
+  }
+  
+  else {
+    digitalWrite(13, LOW);
+  }
+}
---
+++ src/RF433-SOLDERED.cpp
@@ -0,0 +1,21 @@
+/**
+ **************************************************
+ *
+ * @file        Generic-easyC-SOLDERED.cpp
+ * @brief       Example functions to overload in base class.
+ *
+ *
+ * @copyright   GNU General Public License v3.0
+ * @authors     @ soldered.com, Robert Peric
+ ***************************************************/
+
+
+#include "RF433-SOLDERED.h"
+
+/**
+ * @brief                   Sensor specific native constructor.
+ *
+ */
+RF433::RF433()
+{
+}
