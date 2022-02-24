8a6f1aec1fbc98bad0e30e6d858270c65e9e051a
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-02-24 14:59:27 +0100

    Update RHHardwareSPI.cpp

--- src/libs/RadioHead/RHHardwareSPI.cpp
+++ src/libs/RadioHead/RHHardwareSPI.cpp
@@ -46,14 +46,14 @@ uint8_t RHHardwareSPI::transfer(uint8_t data)
 
 void RHHardwareSPI::attachInterrupt() 
 {
-#if (RH_PLATFORM == RH_PLATFORM_ARDUINO || RH_PLATFORM == RH_PLATFORM_NRF52)
+#if ((RH_PLATFORM == RH_PLATFORM_ARDUINO || RH_PLATFORM == RH_PLATFORM_NRF52) && !(ARDUINO_AVR_ATtinyxy4))
     SPI.attachInterrupt();
 #endif
 }
 
 void RHHardwareSPI::detachInterrupt() 
 {
-#if (RH_PLATFORM == RH_PLATFORM_ARDUINO || RH_PLATFORM == RH_PLATFORM_NRF52)
+#if ((RH_PLATFORM == RH_PLATFORM_ARDUINO || RH_PLATFORM == RH_PLATFORM_NRF52) && !(ARDUINO_AVR_ATtinyxy4))
     SPI.detachInterrupt();
 #endif
 }
